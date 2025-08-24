29b1948407be10dfdfb822ceec7513ac1cbe2215
ashxvegeta <ashutoshroy453@gmail.com>
2025-08-24 21:24:46 +0530

    varibales

---
+++ variables.py
@@ -0,0 +1,16 @@
+name = "Ashu"
+age = 23
+price = 100.50
+age2 = age
+
+print("Name:", name)
+print("Age:", age)
+print("Price:", price)
+print("Age2:", age2)
+
+# print type
+
+print("Type of Name:", type(name))
+print("Type of Age:", type(age))
+print("Type of Price:", type(price))
+print("Type of Age2:", type(age2))
