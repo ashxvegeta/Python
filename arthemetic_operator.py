b88ede6adc09dc235e514375ecdf959ee8035c20
ashxvegeta <ashutoshroy453@gmail.com>
2025-08-26 22:33:53 +0530

    arthememtic operato

---
+++ arthemetic_operator.py
@@ -0,0 +1,41 @@
+x = 1
+y = 2
+z = x + y
+print(z)
+
+a = 5
+b = 4
+c = a - b
+print(c)
+
+a10 = 2
+a20 = 5
+print(a10 * a20)
+
+
+a33 = 10
+a22 =  2
+print(a33/a22)
+
+a33 = 10
+a22 =  2
+print(a33**a22)
+
+a41 = 9
+a42 = 3
+print(a41//a42)
+
+
+a51 = 10
+a52 = 3
+print(a51%a52)
+
+
+b1 = 10
+b2 = 3
+result = (b1 % b2) * 2
+print(result)
+
+
+num = input("Enter a number: ")
+print(int(num)-5)
