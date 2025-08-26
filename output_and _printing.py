ef9f41214b515d2d76b4c51bd8e1522b486e13ba
ashxvegeta <ashutoshroy453@gmail.com>
2025-08-26 22:00:30 +0530

    output and printing

---
+++ output_and _printing.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+print(4.5)
+print(10,"hello")
+print('1.5',"hello")
+print("hello",'end',78,False)
+print("hellox",'endx',78,False ,end='|')
+print("hello")
+print("hellox",'endx',78,False ,end='\n')  #move to next line
+print("hello")
