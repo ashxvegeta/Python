3136538e804ea5b396e0772de19544f9c4bbab94
ashxvegeta <ashutoshroy453@gmail.com>
2025-08-27 00:33:10 +0530

    string methods

---
+++ string_method.py
@@ -0,0 +1,9 @@
+hello = "hello"
+print(type(hello)) 
+
+print(hello.upper())  # Convert to uppercase
+
+hello2 = "HELLO"
+print(hello2.lower())  # Convert to lowercase
+print(hello2.capitalize())  # Capitalize the first letter
+print(hello2.count('L'))  # Count occurrences of 'L'
