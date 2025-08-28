94b642ac1e58b83ad8c7eeed3d14cbf73b700ba5
ashxvegeta <ashutoshroy453@gmail.com>
2025-08-29 00:00:29 +0530

    conditional operators

---
+++ conditional_opeartors.py
@@ -0,0 +1,13 @@
+x = 'hello'
+y = 'hello'
+print(x == y)  # True
+
+
+x1 = 'hello'
+y1 = 'hello'
+print(x1 != y1)  # False
+
+
+print(5 > 3)  # True
+print(5 < 3)  # False
+print('a' > 'b')  # False
