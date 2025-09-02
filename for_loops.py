0594dd81d305087bf7d5a6df7d5a18497903276e
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-02 22:39:09 +0530

    print list

--- for_loops.py
+++ for_loops.py
@@ -14,3 +14,11 @@ for i in range(1, 11):
 print("Print 1 to 10")
 for i in range(1, 11, 2):
     print(i)
+
+
+
+# now print list
+print("Print list")
+my_list = [1, 2, 3, 4, 5]
+for i in my_list:
+    print(i)
