933c01ed2c22480370f51ba8dd6d131ecd727189
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-02 23:03:47 +0530

    Print 0 to 9 using break

--- while_loops.py
+++ while_loops.py
@@ -4,3 +4,12 @@ i = 0
 while i < 10:
     print(i)
     i += 1
+
+# print 0 to 9 using break
+print("Print 0 to 9 using break")
+i = 0
+while True:
+    print(i)
+    i += 1
+    if i == 10:
+        break
