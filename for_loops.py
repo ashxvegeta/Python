e91a55d079c0ff0ac1c2a908a7166324edb0de6f
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-02 22:24:13 +0530

    for loops

---
+++ for_loops.py
@@ -0,0 +1,16 @@
+# for 0 to 9
+print("Print 0 to 9")
+for i in range(10):
+    print(i)
+
+
+# Print  1 to 10
+print("Print 1 to 10")
+for i in range(1, 11):
+    print(i)
+
+
+# Print 1 to 10
+print("Print 1 to 10")
+for i in range(1, 11, 2):
+    print(i)
