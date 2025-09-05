3507c8b515981ef364f0e817237079b6b36650a6
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-05 23:22:09 +0530

    functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+def func():
+    print("Hello, World!")
+
+func()
+
+# 2
+
+def func_with_args(a, b):
+    return a + b
+
+func_with_args(5, 10)
+print(func_with_args(5, 10))
+# output: (0, 1, 2, 3, 4)
