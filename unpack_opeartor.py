99d5bce6041fe0f9459e352b7fc9c9394c9c1a9e
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-07 12:44:39 +0530

    print using unpacking dictionaries

--- unpack_opeartor.py
+++ unpack_opeartor.py
@@ -22,3 +22,9 @@ def func3(x,y):
 pairs = [(1, 2), (3, 4), (5, 6)]
 for p in pairs:
     func3(*p)
+
+# dictionaries
+def func4(x,y):
+    print(x, y)
+# print using unpacking dictionaries
+pairs = [{'x': 1, 'y': 2}, {'x': 3, 'y': 4}, {'x': 5, 'y': 6}]
