bba0977f3148b8fd401eb75d31b9849331d0277c
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-07 12:35:06 +0530

    print using unpacking using loops

--- unpack_opeartor.py
+++ unpack_opeartor.py
@@ -11,3 +11,14 @@ def funcx(*args, **kwargs):
 
 x = [1, 23, 22223, 2224]
 print(*x)
+print(x)
+
+
+#print using unpacking 
+
+def func3(x,y):
+    print(x, y)
+
+pairs = [(1, 2), (3, 4), (5, 6)]
+for p in pairs:
+    func3(*p)
