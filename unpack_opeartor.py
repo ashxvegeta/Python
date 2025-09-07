325a9b848c2d58d163e318d662851f0e0e34aa52
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-07 12:29:21 +0530

    unpack operator and argument

--- unpack_opeartor.py
+++ unpack_opeartor.py
@@ -1,5 +1,13 @@
-def func(x):
-    def func2():
-        print(x)
-    return func2
-func(10)()
+# def func(x):
+#     def func2():
+#         print(x)
+#     return func2
+# func(10)()
+
+
+# arguments unpacking
+def funcx(*args, **kwargs):
+    pass
+
+x = [1, 23, 22223, 2224]
+print(*x)
