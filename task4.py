6dcc72b9875874ccca4a271f7ad3f6856c4e3d35
ashxvegeta <ashutoshroy453@gmail.com>
2025-09-08 22:21:30 +0530

    Task4

--- task4.py
+++ task4.py
@@ -21,4 +21,20 @@ students = [
 for student in students:
     toatl = sum(student["marks"])
     avg = toatl / len(student["marks"])
-    print(student["name"], "→", round(avg, 1))
+    # print(student["name"], "→", round(avg, 1))
+
+
+# function
+
+
+def print_average_marks(students):
+    for student in students:
+        total = sum(student["marks"])
+        avg = total / len(student["marks"])
+        print(student["name"], "→", round(avg, 1))
+studentsname = [
+    {"name": "Ashu", "marks": [80, 90, 85]},
+    {"name": "Riya", "marks": [70, 60, 75]}
+]
+
+print(print_average_marks(studentsname))
